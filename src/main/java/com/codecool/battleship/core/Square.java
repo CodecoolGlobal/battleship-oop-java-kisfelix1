aa04ebbe258af8250499b8b37ad45a7e44e37e30
Kis Béla <kisfelix1@gmail.com>
2021-11-29 14:33:51 +0100

    Feature - create Square - SquareStatus field

--- src/main/java/com/codecool/battleship/core/Square.java
+++ src/main/java/com/codecool/battleship/core/Square.java
@@ -1,9 +1,12 @@
 class Square {
     private final int x;
     private final int y;
+    private SquareStatus status;
 
     public Square(x, y) {
         this.x = x;
         this.y = y;
+        status = SquareStatus.Empty;
     }
+
 }
