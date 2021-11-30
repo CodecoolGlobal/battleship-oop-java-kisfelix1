b7565c45920e1b01996d4f7eddc6c77734c42bdc
Csipke József <csipkej98@gmail.com>
2021-11-30 16:28:44 +0100

    Feature - Input 'isValidCoordinateForm' implemented

--- src/main/com/codecool/battleship/utility/Input.java
+++ src/main/com/codecool/battleship/utility/Input.java
@@ -6,11 +6,11 @@ public class Input {
     Scanner inputScan = new Scanner(System.in);
     Display display = new Display();
 
-public String askPlacementCoordinate() {
+public String askPlacementCoordinate(String shipType) {
     while (true) {
-        display.println("Where do you want to place the (PLACEHOLDER FOR SHIPTYPE) ship?");
+        display.println("Where do you want to place the "+ shipType +" ship?");
         String userInput = inputScan.nextLine();
-        if (validateCoordinate(userInput)) {
+        if (isValidCoordinateForm(userInput)) {
             return userInput;
         }
     }
@@ -20,8 +20,27 @@ public String askAttackCoordinate(int player) {
     String userInput = inputScan.nextLine();
     return userInput;
 }
-public boolean validateCoordinate(String userInput) {
-    // VALIDATE NEEDED
-    return true;
+public boolean isValidCoordinateForm(String userInput) {
+    String reorderedInput = "";
+    String charInputs = "";
+    String integerInputs = "";
+    for (char character : userInput.toUpperCase().toCharArray()) {
+        if (Character.isDigit(character)) {
+            integerInputs += character;
+        } else {
+            charInputs += character;
+        }
+    }
+    reorderedInput += charInputs + integerInputs;
+    char[] inputCharArray = reorderedInput.toCharArray();
+    if (reorderedInput.length() == 2 || reorderedInput.length() == 3) {
+        if (Character.isAlphabetic(inputCharArray[0])) {
+            return reorderedInput.length() == 2 ?
+                    Character.isDigit(inputCharArray[1]) :
+                    Character.isDigit(inputCharArray[1])
+                            && Character.isDigit(inputCharArray[2]);
+        }
+    }
+    return false;
 }
 }
