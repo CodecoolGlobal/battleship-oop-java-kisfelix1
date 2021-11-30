92f271996e4eed82580c71f6b854f7123b29bdc0
Kis Béla <kisfelix1@gmail.com>
2021-11-30 13:13:28 +0100

    Feature - create advanced game cycle

--- src/main/com/codecool/battleship/Game.java
+++ src/main/com/codecool/battleship/Game.java
@@ -2,9 +2,12 @@ package com.codecool.battleship;
 
 import com.codecool.battleship.core.Board;
 import com.codecool.battleship.core.BoardFactory;
+import com.codecool.battleship.utility.*;
 
 
 public class Game {
+    private static final Display display = new Display();
+    private static final Input input = new Input();
     private Board board;
 
     public Game() {
@@ -18,9 +21,16 @@ public class Game {
     }
 
     private void gameCycle() {
-        while(!checkWin()){
+        while (!checkWin()) {
+            display.printBoard(10);
+            playerShoot();
+            //changeCurrentPlayer();
 
         }
+    }
+
+    private void playerShoot() {
+        input.askAttackCoordinate(1);
     }
 
     private boolean checkWin() {
--- src/main/com/codecool/battleship/utility/Display.java
+++ src/main/com/codecool/battleship/utility/Display.java
@@ -6,27 +6,31 @@ public class Display {
     public void println(String print) {
         System.out.println(print);
     }
+
     public void print(String print) {
         System.out.print(print);
     }
+
     public void printMainMenu() {
         println("[1]Start new game" +
                 "[2]Start new game with different game mode" +
                 "[3]Exit");
     }
+
     public void printBoard(int boardSize) {
         String finalBoard = "";
         String boardCordNumbers = "";
         String boardTopBottom = "";
-        for (int row = 0; row < boardSize+1; row++) {   //Generates the top and bottom part of the board, according to
+        for (int row = 0; row < boardSize + 1; row++) {   //Generates the top and bottom part of the board, according to
             if (row == 0) {                             //the board size
                 boardCordNumbers += "    ";
                 boardTopBottom += "  - ";
-            } if (row == boardSize) {
+            }
+            if (row == boardSize) {
                 boardCordNumbers += " \n";
                 boardTopBottom += "- \n";
             } else {
-                boardCordNumbers += (row+1)+" ";
+                boardCordNumbers += (row + 1) + " ";
                 boardTopBottom += "- ";
             }
         }
@@ -34,20 +38,20 @@ public class Display {
         for (int row = 0; row < boardSize; row++) {
             for (int col = 0; col < boardSize; col++) {
                 if (col == 0) {
-                    finalBoard += row == 0 ? (char)(ASCII_CHARACTER_A+row)+" | " : "|\n" + (char)(ASCII_CHARACTER_A+row)+" | ";
+                    finalBoard += row == 0 ? (char) (ASCII_CHARACTER_A + row) + " | " : "|\n" + (char) (ASCII_CHARACTER_A + row) + " | ";
                 }
                 finalBoard += ". ";
-                 if (row == boardSize - 1 && col == boardSize-1) {
+                if (row == boardSize - 1 && col == boardSize - 1) {
                     finalBoard += "|";
                 }
                 // NEED BOARD(OCEAN)
             }
         }
-        finalBoard += "\n"+boardTopBottom;
+        finalBoard += "\n" + boardTopBottom;
         System.out.println(finalBoard);
     }
 
     public void printWinner(int player) {
-        println("Player "+ player + " has won the game!");
+        println("Player " + player + " has won the game!");
     }
 }
