e5ead5d10fd380eb08728421d0f4f561a2c5a2f5
Kis Béla <kisfelix1@gmail.com>
2021-12-01 12:32:00 +0100

    Feature - main menu done

--- src/main/com/codecool/battleship/Battleship.java
+++ src/main/com/codecool/battleship/Battleship.java
@@ -1,8 +1,32 @@
 package com.codecool.battleship;
 
+import com.codecool.battleship.utility.Display;
+import com.codecool.battleship.utility.Input;
+
 public class Battleship {
     public static void main(String[] args) {
-        Game game = new Game();
-        game.gameCycle();
+        Input input = new Input();
+        Display display = new Display();
+        boolean quit = false;
+        while (!quit) {
+            display.printMainMenu();
+            String choice = input.askUserInput();
+            startMenuOption(choice);
+        }
+    }
+
+    private static void startMenuOption(String choice) {
+        switch(choice){
+            case "1":
+                Game game = new Game();
+                game.gameCycle();
+                break;
+            case "2":
+                System.out.println("Other option");
+                break;
+            case "3":
+                System.exit(0);
+                break;
+        }
     }
 }
--- src/main/com/codecool/battleship/utility/Input.java
+++ src/main/com/codecool/battleship/utility/Input.java
@@ -6,22 +6,28 @@ public class Input {
     Scanner inputScan = new Scanner(System.in);
     Display display = new Display();
 
-public String askPlacementCoordinate() {
-    while (true) {
-        display.println("Where do you want to place the (PLACEHOLDER FOR SHIPTYPE) ship?");
-        String userInput = inputScan.nextLine();
-        if (validateCoordinate(userInput)) {
-            return userInput;
+    public String askPlacementCoordinate() {
+        while (true) {
+            display.println("Where do you want to place the (PLACEHOLDER FOR SHIPTYPE) ship?");
+            String userInput = inputScan.nextLine();
+            if (validateCoordinate(userInput)) {
+                return userInput;
+            }
         }
     }
-}
-public String askAttackCoordinate(int player) {
-    display.println("Player " + player + ", input an attack coordinate!");
-    String userInput = inputScan.nextLine();
-    return userInput;
-}
-public boolean validateCoordinate(String userInput) {
-    // VALIDATE NEEDED
-    return true;
-}
+
+    public String askAttackCoordinate(int player) {
+        display.println("Player " + player + ", input an attack coordinate!");
+        String userInput = inputScan.nextLine();
+        return userInput;
+    }
+
+    public boolean validateCoordinate(String userInput) {
+        // VALIDATE NEEDED
+        return true;
+    }
+
+    public String askUserInput(){
+        return inputScan.nextLine();
+    }
 }
