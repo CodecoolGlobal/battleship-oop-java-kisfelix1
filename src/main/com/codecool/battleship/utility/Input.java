f4fe6759372d7048f3a109226a8d112190b4d81d
Kis Béla <kisfelix1@gmail.com>
2021-12-01 15:14:22 +0100

    Other - merge

--- src/main/com/codecool/battleship/utility/Input.java
+++ src/main/com/codecool/battleship/utility/Input.java
@@ -10,13 +10,14 @@ public class Input {
 
     public String askPlacementCoordinate(String shipType) {
         while (true) {
-            display.println("Where do you want to place the "+ shipType +" ship?");
+            display.println("Where do you want to place the " + shipType + " ship?");
             String userInput = inputScan.nextLine();
             if (isValidCoordinateForm(userInput)) {
                 return userInput;
             }
         }
     }
+
     public String askAttackCoordinate(int player, Board attackedBoard) {
         display.println("Player " + player + ", input an attack coordinate!");
         String userInput = inputScan.nextLine();
@@ -27,6 +28,7 @@ public class Input {
         }
         return userInput;
     }
+
     public boolean isValidCoordinateForm(String userInput) {
         String reorderedInput = reorderPlayerInputs(userInput);
         char[] inputCharArray = reorderedInput.toCharArray();
@@ -45,13 +47,14 @@ public class Input {
         int upperAAscii = 65;
         String reorderedInput = reorderPlayerInputs(userInput);
         char[] inputCharArray = reorderedInput.toCharArray();
-        int firstCoordinate = (int)inputCharArray[0]-upperAAscii;
-        int secondCoordinate = (userInput.length() == 2 ? inputCharArray[1] : inputCharArray[1] + inputCharArray[2])-1;
+        int firstCoordinate = (int) inputCharArray[0] - upperAAscii;
+        int secondCoordinate = (userInput.length() == 2 ? inputCharArray[1] : inputCharArray[1] + inputCharArray[2]) - 1;
         return firstCoordinate >= 0 &&
                 firstCoordinate < attackedBoard.getOcean().length &&
                 secondCoordinate >= 0 &&
                 secondCoordinate < attackedBoard.getOcean().length;
     }
+
     public String reorderPlayerInputs(String userInput) {
         String charInputs = "";
         String integerInputs = "";
@@ -65,7 +68,7 @@ public class Input {
         return charInputs + integerInputs;
     }
 
-    public String askUserInput(){
+    public String askUserInput() {
         return inputScan.nextLine();
     }
 }
