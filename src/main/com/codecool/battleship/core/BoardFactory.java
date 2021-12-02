9cce6bb8977ffb651c6f476343716f1a44fe5d83
Kis Béla <kisfelix1@gmail.com>
2021-12-02 13:50:01 +0100

    Refactor - refactor placement in boardFactory 3.0

--- src/main/com/codecool/battleship/core/BoardFactory.java
+++ src/main/com/codecool/battleship/core/BoardFactory.java
@@ -25,13 +25,11 @@ public class BoardFactory {
             randomPlacement();
         } else {
             String placementChoice = input.askUserInput("Choose placement method:\n[1] - manual\n[2] - random");
-            if (placementChoice.equals("1")){
+            if (placementChoice.equals("1")) {
                 manualPlacement();
-            }
-            else if(placementChoice.equals("2")){
+            } else if (placementChoice.equals("2")) {
                 randomPlacement();
-            }
-            else{
+            } else {
                 placeShips(player);
             }
         }
@@ -65,27 +63,30 @@ public class BoardFactory {
         int[] shipCoordinates = null;
         Ship ship = null;
         String direction = null;
-        do{
+        do {
             display.printBoard(board, true);
             direction = getPlacementDirection();
             String placement = input.askPlacementCoordinate(type);
             shipCoordinates = input.convertStringToMove(placement);
             ship = new Ship(type);
-        }while(!board.isPlacementOk(shipCoordinates[0], shipCoordinates[1], ship, direction));
+        } while (!board.isPlacementOk(shipCoordinates[0], shipCoordinates[1], ship, direction));
         placeShip(shipCoordinates[0], shipCoordinates[1], ship, direction);
     }
 
     private void tryRandomPlacement(ShipType type) {
-        int directionRandom = rand.nextInt(2);
-        String direction = directionRandom == 1 ? "horizontal" : "vertical";
-        int x = rand.nextInt(board.getOcean().length - type.shipSize);
-        int y = rand.nextInt(board.getOcean().length - type.shipSize);
-        Ship ship = new Ship(type);
-        if (board.isPlacementOk(x, y, ship, direction)) {
-            placeShip(x, y, ship, direction);
-        } else {
-            tryRandomPlacement(type);
-        }
+        int directionRandom = 0;
+        String direction = null;
+        int x = 0;
+        int y = 0;
+        Ship ship;
+        do {
+            directionRandom = rand.nextInt(2);
+            direction = directionRandom == 1 ? "horizontal" : "vertical";
+            x = rand.nextInt(board.getOcean().length - type.shipSize);
+            y = rand.nextInt(board.getOcean().length - type.shipSize);
+            ship = new Ship(type);
+        }while (!board.isPlacementOk(x, y, ship, direction));
+        placeShip(x, y, ship, direction);
     }
 
     private void placeShip(int fromXCoordinate, int fromYCoordinate, Ship ship, String direction) {
